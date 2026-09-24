6e497728d44fd54eac94ee7e16ca25496f4df60f
Kelly Nezat <Soullab1@gmail.com>
2026-09-23 20:56:31 -0400

    feat(writers-studio): open isolated light-shell flagship v2 preview

---
+++ app/writers-studio/flagship-v2/page.tsx
@@ -0,0 +1,7 @@
+import FlagshipV2Preview from './FlagshipV2Preview';
+
+export const dynamic = 'force-dynamic';
+
+export default function WriterStudioFlagshipV2PreviewPage() {
+  return <FlagshipV2Preview />;
+}
